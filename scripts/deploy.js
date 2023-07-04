b0ef2088a19825547d5f719e7cc688c3217cca49
Pedro Magalhães <pedro@scrumlaunch.com>
2023-07-04 11:48:01 -0300

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,27 +7,26 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  const [deployer] = await hre.ethers.getSigners();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    "Deploying contracts with the account:",
+    deployer.address
   );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Token = await hre.ethers.getContractFactory("RealDigital");
+  const token = await Token.deploy("RealDigital", "RD", deployer.address);
+
+  await token.deployed();
+
+  console.log("Token deployed to:", token.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
