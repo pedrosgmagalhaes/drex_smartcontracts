711fbe221b072e5e592e574f85cc5deb1dda37a9
Krzysztof Cywiński <k.m.cywinski@gmail.com>
2023-09-21 16:23:13 +0200

    Added dotenv. Added BESU config to hardhat.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -7,8 +8,15 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
+        runs: 200,
       },
+    },
+  },
+  networks: {
+    hardhat: {},
+    besuPrivate: {
+      url: process.env.BESU_PRIVATE_URL ?? "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
