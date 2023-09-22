9a26eecb78abe9724788931a37bdbef4e6dcfd41
Krzysztof Cywiński <k.m.cywinski@gmail.com>
2023-09-22 19:43:14 +0200

    Initial monolith deployment script.

--- util/constants.js
+++ util/constants.js
@@ -2,8 +2,30 @@ const REAL_NAME = "Real Digital";
 const REAL_SYMBOL = "BRL";
 const REAL_DECIMALS = 2;
 
+const CONTRACTS = {
+  ADDRESS_DISCOVERY: "AddressDiscovery",
+  REAL_DIGITAL: "RealDigital",
+  REAL_DIGITAL_DEFAULT_ACCOUNT: "RealDigitalDefaultAccount",
+  REAL_DIGITAL_ENABLE_ACCOUNT: "RealDigitalEnableAccount",
+  REAL_TOKENIZADO_12345678: "RealTokenizado",
+  REAL_TOKENIZADO_87654321: "RealTokenizado",
+  STR: "STR",
+  SWAP_ONE_STEP: "SwapOneStep",
+  SWAP_TWO_STEPS: "SwapTwoSteps",
+  KEY_DICTIONARY: "KeyDictionary",
+};
+
+const ACCOUNTS = {
+  ADMIN: 0,
+  AUTHORITY: 1,
+  PARTICIPANT_12345678_DEFAULT: 2,
+  PARTICIPANT_87654321_DEFAULT: 3,
+};
+
 module.exports = {
     REAL_NAME,
     REAL_SYMBOL,
-    REAL_DECIMALS
+    REAL_DECIMALS,
+    CONTRACTS,
+    ACCOUNTS,
 }
